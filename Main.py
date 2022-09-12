fdb07570e0d963296bd7454a8eda3bf4d9aacaf9
NIMITHA <101205074+NIMITHA-4203@users.noreply.github.com>
2022-09-12 15:15:50 +0530

    Second Commit

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from typing import List
 
 def selectionSort(array, size) -> List[int]:  
   
-  for indexo in range(size):
+  for indexo in range(size-1):
     small_index = indexo
     for index in range(indexo + 1,size):
       if array[index] < array[small_index]:
